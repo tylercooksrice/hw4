0d186200e7db9c42a980d5abe0bca914f5df7947
tylercooksrice <darksilverarrow@gmail.com>
2025-03-02 23:32:24 -0800

    213412

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,1 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleSwitch = document.getElementById("theme-toggle");
+    const currentTheme = localStorage.getItem("theme");
 
+    if (currentTheme) {
+        document.documentElement.setAttribute("data-theme", currentTheme);
+        if (currentTheme === "dark") {
+            toggleSwitch.checked = true;
+        }
+    }
+
+    toggleSwitch.addEventListener("change", function () {
+        if (this.checked) {
+            document.documentElement.setAttribute("data-theme", "dark");
+            localStorage.setItem("theme", "dark");
+        } else {
+            document.documentElement.setAttribute("data-theme", "light");
+            localStorage.setItem("theme", "light");
+        }
+    });
+});
